03d87e9d80a3b86462c0cac8057449bf4796e214
Gabriel <ionut.guita@gmail.com>
2016-10-27 18:57:21 +0200

    adding controller

--- madlibs/myApp.js
+++ madlibs/myApp.js
@@ -7,4 +7,39 @@ angular.module('myApp', [])
 		female: 'female'
 	};
 
+	// $scope.maleName = function(){
+	// 	return maleName;
+	// }
+
+	// $scope.dirtyTask = function(){
+	// 	return $scope.dirtyTask;
+	// }
+
+	// $scope.obnoxiousCelebrity = function(){
+	// 	return $scope.obnoxiousCelebrity;
+	// }
+	
+	// $scope.jobTitle = function(){
+	// 	return $scope.jobTitle;
+	// }
+
+	// $scope.celebrity = function(){
+	// 	return $scope.celebrity;
+	// }
+
+	// $scope.hugeNumber = function(){
+	// 	return $scope.hugeNumber;
+	// }
+
+	// $scope.tediousTask = function(){
+	// 	return $scope.tediousTask;
+	// }
+
+	// $scope.uselessSkill = function(){
+	// 	return $scope.uselessSkill;
+	// }
+
+	// $scope.adjective = function(){
+	// 	return $scope.adjective;
+	// }
 }]);
