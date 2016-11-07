927006d476c595999fa89188b134b9964635cc2c
Gabriel <ionut.guita@gmail.com>
2016-11-07 23:03:32 +0100

    Multiplication Table V2: Dynamic Upper Limits for Table

--- multiplication_tv1/myApp.js
+++ multiplication_tv1/myApp.js
@@ -1,7 +1,23 @@
 // gabriel
 
 angular.module('myApp', [])
-    .controller('MultiplicationCtrl', function() {
+    .controller('MultiplicationCtrl', function($attrs) {
         var vm = this;
-        vm.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        //vm.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        vm.numberLimit = $attrs.initialNumberLimit || 10;
+
+        vm.numbers = function (){
+        	var numbers = [];
+        	for (var i = 0; i < vm.numberLimit; i++) {
+        		numbers[i] = i + 1;
+        	}
+
+        	return numbers;
+        }
+
+        vm.compute = function(a, b){
+        	return a * b;
+        }
+
     });
