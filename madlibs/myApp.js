1af030ef1c54a4f1818ed3bd7d7ad0d2e71a7cdb
Gabriel <ionut.guita@gmail.com>
2016-11-07 21:40:24 +0100

    Mad Libs Refactor 2: Controller with Controller As

--- madlibs/myApp.js
+++ madlibs/myApp.js
@@ -2,44 +2,8 @@
 angular.module('myApp', [])
 .controller('madlibs', ['$scope', function($scope) {
 
-	$scope.gender = {
-		male: 'male',
-		female: 'female'
-	};
+	var vm = this;
 
-	// $scope.maleName = function(){
-	// 	return maleName;
-	// }
+	vm.user = {};
 
-	// $scope.dirtyTask = function(){
-	// 	return $scope.dirtyTask;
-	// }
-
-	// $scope.obnoxiousCelebrity = function(){
-	// 	return $scope.obnoxiousCelebrity;
-	// }
-	
-	// $scope.jobTitle = function(){
-	// 	return $scope.jobTitle;
-	// }
-
-	// $scope.celebrity = function(){
-	// 	return $scope.celebrity;
-	// }
-
-	// $scope.hugeNumber = function(){
-	// 	return $scope.hugeNumber;
-	// }
-
-	// $scope.tediousTask = function(){
-	// 	return $scope.tediousTask;
-	// }
-
-	// $scope.uselessSkill = function(){
-	// 	return $scope.uselessSkill;
-	// }
-
-	// $scope.adjective = function(){
-	// 	return $scope.adjective;
-	// }
 }]);
