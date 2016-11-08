4fed505f1511ba5ce3a7977927ac8befe8d8f3ac
Gabriel <ionut.guita@gmail.com>
2016-11-08 22:31:44 +0100

    Mad Libs App Refactor 3: A Better User Experience

--- madlibs/myApp.js
+++ madlibs/myApp.js
@@ -3,7 +3,20 @@ angular.module('myApp', [])
 .controller('madlibs', ['$scope', function($scope) {
 
 	var vm = this;
-
 	vm.user = {};
+
+	vm.inputScreen = true;
+	vm.resultScreen = false;
+
+	vm.sendData = function(){
+		vm.inputScreen = false;
+		vm.resultScreen = true;
+	}
+
+	vm.startOVer = function () {
+		vm.inputScreen = true;
+		vm.resultScreen = false;
+		vm.user = {};
+	}
 
 }]);
