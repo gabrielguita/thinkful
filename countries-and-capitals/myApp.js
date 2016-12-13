10b11983d6ab5661656fed51f795ef1b9634435b
unknown <gabriel.guita@geodan.nl>
2016-12-13 14:09:07 +0100

    cleaning code

--- countries-and-capitals/myApp.js
+++ countries-and-capitals/myApp.js
@@ -1,7 +1,6 @@
-
 angular.module('myApp', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', { //test
+        $routeProvider.when('/', {  
             templateUrl : 'home.html',
             controller : 'HomeCtrl',
             controllerAs: 'vm'
@@ -21,16 +20,6 @@ angular.module('myApp', ['ngRoute'])
 		        }
 		    }
 		})
-        .when('/new-meal', {
-            templateUrl : 'new-meal.html',
-            controller : 'NewMealCtrl',
-            controllerAs: 'vm'
-        })
-        .when('/my-earnings', {
-            templateUrl : 'my-earnings.html',
-            controller : 'MyEarningsCtrl',
-        	controllerAs: 'vm'
-        })
         .when('/countries', {
             templateUrl : 'countries.html',
             controller : 'countriesCtrl',
@@ -63,47 +52,7 @@ angular.module('myApp', ['ngRoute'])
     }])
     .controller('HomeCtrl', [function() {
         //console.log('dasda');
-    }])
-    .controller('NewMealCtrl',  ['dataService', function(dataService) {
-		var vm = this;
-		vm.bill = {};
-		
-		vm.bill.subTotal = '0.00';
-		vm.bill.tip = '0.00';
-		vm.bill.total = '0.00';
-		vm.bill.tipTotal = '0.00';
-		vm.bill.averageTipPerMeal = '0.00';
-
-	 	vm.bill.mealCounter = 0;
-
-
-		// vm.btnSubmit = function(mealCount){
-
-		// 	vm.bill.percentValue = (vm.bill.taxeRate / 100) * vm.bill.baseMealPrice;
-
-		// 	vm.bill.subTotal = vm.bill.percentValue + vm.bill.baseMealPrice;
-
-		// 	vm.bill.tip = (vm.bill.tipPergentage / 100) * vm.bill.subTotal;
-
-		// 	vm.bill.total = vm.bill.subTotal + vm.bill.tip;
-
-		// 	vm.bill.mealCounter += mealCount;
-
-		// 	vm.bill.tipTotal = vm.bill.tip * vm.bill.mealCounter;
-
-		// 	vm.bill.averageTipPerMeal = vm.bill.tipTotal / vm.bill.mealCounter;	 
-			
-		// 	dataService.set(vm.bill);
-		// 	//$scope.$broadcast('billValue', vm.bill);
-		// }
-
-		// vm.btnCancel = function () {
-		// 	vm.bill.baseMealPrice = '';
-		// 	vm.bill.taxeRate = ''
-		// 	vm.bill.tipPergentage = '';
-		// }
-    }])
- 
+    }]) 
      //  .service('dataService', ['$http', function ($http) {
 
      //    var urlBase = 'http://api.geonames.org/countryInfoJSON?username=gabrielgi';
@@ -124,6 +73,5 @@ angular.module('myApp', ['ngRoute'])
     	return dataFactory;
 	}]) 
     .config(['$httpProvider', function ($httpProvider) {
-            // enable http caching
-           $httpProvider.defaults.cache = true;
+		$httpProvider.defaults.cache = true;
     }]);
